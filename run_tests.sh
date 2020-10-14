fdf50cc969f621a63fcf581065fa9eb1aed6dc3e
Kelvin Santos <kelvinsteinersantos@gmail.com>
2020-10-14 18:19:00 -0300

    script de teste que ficou para trás

--- run_tests.sh
+++ run_tests.sh
@@ -8,6 +8,8 @@ tests_base="./tests"
 mkdir -p "${tests_base}/input"
 mkdir -p "${tests_base}/output"
 mkdir -p "${tests_base}/tokens"
+mkdir -p "${tests_base}/output-expected"
+mkdir -p "${tests_base}/tokens-expected"
 
 for test_file in "${tests_base}"/input/*.c; do
 
@@ -15,11 +17,23 @@ for test_file in "${tests_base}"/input/*.c; do
     echo "Testing '${basename}'"
 
     tokens_file="${tests_base}/tokens/${basename}.tokens.txt"
-    < "${test_file}" "${dump_tokens_exe}" > "${tokens_file}" || true
-
-    output_file="${tests_base}/output/${basename}.txt"
+    output_file="${tests_base}/output/${basename}.out.txt"
     err_file="${tests_base}/output/${basename}.err.txt"
-    < "${test_file}" "${exe}" 2> >(tee "${err_file}" >&2) > >(tee "${output_file}") || true
 
-    echo
+    tokens_expected_file="${tests_base}/tokens-expected/${basename}.tokens.txt"
+    output_expected_file="${tests_base}/output-expected/${basename}.out.txt"
+    err_expected_file="${tests_base}/output-expected/${basename}.err.txt"
+
+    < "${test_file}" "${dump_tokens_exe}" >"${tokens_file}" 2>&1 || true
+
+    # < "${test_file}" "${exe}" 2> >(tee "${err_file}" >&2) > >(tee "${output_file}") || true
+    < "${test_file}" "${exe}" 2> "${err_file}" > "${output_file}" || true
+
+    touch "${tokens_expected_file}"
+    touch "${output_expected_file}"
+    touch "${err_expected_file}"
+
+    diff "${tokens_expected_file}" "${tokens_file}"
+    diff "${output_expected_file}" "${output_expected_file}"
+    diff "${err_expected_file}" "${err_file}"
 done
